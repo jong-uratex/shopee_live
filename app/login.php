a012238c6dae93760e0f97ea6bf187eca479a673
Jenor Ricafort <80311759+jong-uratex@users.noreply.github.com>
2026-08-11 01:38:48 +0000

    Fixed.
    login.php now redirects to success.php
    login.php now redirects to success.php

--- app/login.php
+++ app/login.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 session_regenerate_id(true);
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['username'] = $user['username'];
-                header('Location: success.php');
+                header('Location: /app/success.php');
                 exit;
             } else {
                 $err = 'Invalid credentials.';
